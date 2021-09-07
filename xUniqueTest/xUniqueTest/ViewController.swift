5224c5df2778764d2d0b9f033b80df499a43232b
beomkeydev <beomkey.dev@kakaoenterprise.com>
2021-09-07 13:35:57 +0900

    [init] 프로젝트 생성
    초기 프로젝트 생성

---
+++ xUniqueTest/xUniqueTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xUniqueTest
+//
+//  Created by beomkey on 2021/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
